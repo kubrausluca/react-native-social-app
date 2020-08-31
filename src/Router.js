5d0ed6016082e2b473e1bc948799e898ee16e468
Kubra Usluca <kubra.usluca.98@gmail.com>
2020-08-31 17:52:19 +0300

    first commit

---
+++ src/Router.js
@@ -0,0 +1,43 @@
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator } from "react-navigation-stack";
+import LoadingScreen from "./screens/LoadingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+import * as firebase from 'firebase';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCWLi2mh8zPBeTPC4_lMQ2Hgnl4a9EcX2o",
+  authDomain: "socialapp-75dc5.firebaseapp.com",
+  databaseURL: "https://socialapp-75dc5.firebaseio.com",
+  projectId: "socialapp-75dc5",
+  storageBucket: "socialapp-75dc5.appspot.com",
+  messagingSenderId: "348349265828",
+  appId: "1:348349265828:web:85c728eeabca432d727331"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const AppStack = createStackNavigator({
+  Home: HomeScreen
+})
+
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen
+})
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: AppStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "Loading"
+    }
+  )
+)
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+export default class LoginScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.greeting}>
+                    {'Hello again.\nWelcome back.'}
+                </Text>
+
+                <View style={styles.errorMessage}>
+                    <Text>Error</Text>
+                </View>
+
+                <View style={styles.form}>
+                    <View>
+                        <Text style={styles.inputTitle}>Email Address</Text>
+                        <TextInput style={styles.input} autoCapitalize="none" keyboardType="email-address"></TextInput>
+                    </View>
+
+                    <View style={{ marginTop: 32 }}>
+                        <Text style={styles.inputTitle}>Password</Text>
+                        <TextInput style={styles.input} secureTextEntry autoCapitalize="none"></TextInput>
+                    </View>
+                </View>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text>Sign In</Text>
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    greeting: {
+        marginTop: 32,
+        fontSize: 18,
+        fontWeight: '400',
+        textAlign: 'center',
+
+    },
+    errorMessage: {
+        height: 72,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 30,
+    },
+    form: {
+        marginBottom: 48,
+        marginHorizontal: 30,
+    },
+    inputTitle: {
+        color: "#8A8F9E",
+        fontSize: 10,
+        textTransform: "uppercase",
+    },
+    input: {
+        borderBottomColor: '#8A8F9E',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        height: 40,
+        fontSize: 15,
+        color: '#161F3D',
+    },
+    button: {
+        marginHorizontal: 30,
+        backgroundColor: '#E9446A',
+        borderRadius: 4,
+        height: 52,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
