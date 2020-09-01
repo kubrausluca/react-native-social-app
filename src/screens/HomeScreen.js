e4a1c49774794ea25c33dd3fdf81d5eed3086bbc
Kubra Usluca <kubra.usluca.98@gmail.com>
2020-09-01 17:10:44 +0300

    Register and Login added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import * as firebae from 'firebase';
 
 export default class HomeScreen extends React.Component {
+    state = {
+        email: "",
+        displayName: ""
+    }
+
+    componentDidMount() {
+        const {email, displayName} = firebae.auth().currentUser;
+
+        this.setState({email, displayName});
+    }
+
+    signOutUser = () => {
+        firebae.auth().signOut();
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Home Screen</Text>
+                <Text>Hi {this.state.email}!</Text>
+
+                <TouchableOpacity style={{marginTop: 32}} onPress = {this.signOutUser}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
             </View>
         )
     }
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import * as firebase from 'firebase';
 
 export default class LoginScreen extends React.Component {
+    state = {
+        email: "",
+        password: "",
+        errorMessage: null
+    }
+
+    handleLogin = () => {
+        const {email, password} = this.state
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch(error => this.setState({errorMessage: error.message}))
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -10,23 +26,44 @@ export default class LoginScreen extends React.Component {
                 </Text>
 
                 <View style={styles.errorMessage}>
-                    <Text>Error</Text>
+                    {this.state.errorMessage && <Text style={styles.error}>{this.state.errorMessage}</Text>}
                 </View>
 
                 <View style={styles.form}>
                     <View>
                         <Text style={styles.inputTitle}>Email Address</Text>
-                        <TextInput style={styles.input} autoCapitalize="none" keyboardType="email-address"></TextInput>
+                        <TextInput 
+                            style={styles.input} 
+                            autoCapitalize="none" 
+                            keyboardType="email-address"
+                            onChangeText = { email => this.setState({ email })}
+                            value = {this.setState.email}
+                        ></TextInput>
                     </View>
 
                     <View style={{ marginTop: 32 }}>
                         <Text style={styles.inputTitle}>Password</Text>
-                        <TextInput style={styles.input} secureTextEntry autoCapitalize="none"></TextInput>
+                        <TextInput 
+                            style={styles.input} 
+                            secureTextEntry 
+                            autoCapitalize="none"
+                            onChangeText = { password => this.setState({ password }) }
+                            value = {this.setState.password}    
+                        ></TextInput>
                     </View>
                 </View>
 
-                <TouchableOpacity style={styles.button}>
-                    <Text>Sign In</Text>
+                <TouchableOpacity style={styles.button} onPress = {this.handleLogin}>
+                    <Text style={{ color: "#fff", fontWeight: "500" }}>Sign In</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                    style={{ alignSelf: "center", marginTop: 32}}
+                    onPress = {() => this.props.navigation.navigate("Register")}
+                >
+                    <Text style={{ color: '#414959', fontSize: 13}}>
+                        New to SocialApp? <Text style={{ fontWeight: '500', color: '#E9446A'}}>Sign Up</Text>
+                    </Text>
                 </TouchableOpacity>
 
             </View>
@@ -50,6 +87,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         marginHorizontal: 30,
+    },
+    error: {
+        color: '#E9446A',
+        fontSize: 13,
+        fontWeight: '600',
+        textAlign: 'center',
     },
     form: {
         marginBottom: 48,
